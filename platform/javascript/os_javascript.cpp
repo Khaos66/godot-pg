0c4b24bb85f14fb5ff8551a8b4cf910da6f7154a
Khaos <khaos@khaos-coders.org>
2020-12-22 22:16:37 +0100

    Fix startup size

--- platform/javascript/os_javascript.cpp
+++ platform/javascript/os_javascript.cpp
@@ -163,7 +163,7 @@ void OS_JavaScript::set_window_size(const Size2 p_size) {
 			return window.devicePixelRatio || 1;
 		});
 		emscripten_set_canvas_element_size(canvas_id.utf8().get_data(), p_size.x * scale, p_size.y * scale);
-		emscripten_set_element_css_size(canvas_id.utf8().get_data(), p_size.x, p_size.y);
+		//emscripten_set_element_css_size(canvas_id.utf8().get_data(), p_size.x, p_size.y);
 	}
 }
 
